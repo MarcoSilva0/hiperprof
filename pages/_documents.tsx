7626458c9a4ad5683f555af0430b551eb69edf35
MarcoSilva0 <marcoantonio02016@outlook.com>
2023-10-16 10:13:48 -0300

    fix: Adjust links

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -6,8 +6,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
